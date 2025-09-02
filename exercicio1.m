c69b9aaeb2d354208cbc605ccd47a0faa05bdf67
David Drummond Wainstein <73289677+Daviddw22@users.noreply.github.com>
2025-09-02 07:53:30 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -7,8 +7,20 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t_roots = zeros(1,imax);
+
+t_roots(1) = x0;
+
+for p = 1:length(t_roots)-1
+  if p ~=1
+    erro(p)=abs((t_roots(p)-t_roots(p-1))/t_roots(p))
+    if erro(p) < es
+      break
+    endif
+  endif
+  t_roots(p+1)=t_roots(p) - (func(t_roots(p)/func_d(t_roots(p))));
+endfor
+t=t_roots(p)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
