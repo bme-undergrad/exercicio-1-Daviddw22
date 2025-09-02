dd5c67bc69ff467c0bf48b5ecdc37db729f1ac31
David Drummond Wainstein <73289677+Daviddw22@users.noreply.github.com>
2025-09-02 10:19:59 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -12,14 +12,14 @@ t_roots = zeros(imax,1);
 t_roots(1) = x0;
 
 for p = 1:length(t_roots)-1
-  if p ~=1
-    erro(p)=abs((t_roots(p)-t_roots(p-1))/t_roots(p))
-    if erro(p) < es
-      break
+    if p ~=1
+        erro(p)=abs((t_roots(p)-t_roots(p-1))/t_roots(p))
+        if erro(p) < es
+            break
+        endif
     endif
-  endif
-  t_roots(p+1)=t_roots(p) - (func(t_roots(p)/func_d(t_roots(p));
-  endfor
+        t_roots(p+1)=t_roots(p) - (func(t_roots(p)/func_d(t_roots(p));
+    endfor
 t=t_roots(p)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
